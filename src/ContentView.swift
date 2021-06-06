61c4aaf3e3d79983b01396bf3e34679e29cd09d1
Nolan Taft <killer123yo@gmail.com>
2021-06-06 13:55:17 -0600

    Add files via upload

---
+++ src/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  Trekr
+//
+//  Created by Nolan Taft on 6/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            // Stacks all assets/visibile identifiers inside it's brackets
+            
+            Image(
+                "highlands"
+            )
+            // Generates an image from assets
+                
+                .resizable() // Allows that image to be stretched/fitted
+                .scaledToFit() // Fits the image to the screen
+            
+            Text(
+                "The Highlands"
+            )
+            // Generates a text
+            
+                .font(
+                    .largeTitle // Changes font and font size to large title and gives spacing around the new title
+                )
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ src/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Nolan Taft on 6/6/21.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
